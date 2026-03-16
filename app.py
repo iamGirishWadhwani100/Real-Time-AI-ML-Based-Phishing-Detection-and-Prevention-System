db18d752e9dd7e4b70b44b8058b6c16d49ec515d
Girish Wadhwani <142938812+iamGirishWadhwani100@users.noreply.github.com>
2026-03-16 21:52:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def tools(tool_name):
     return jsonify({"status": "success", "result": f"Executed tool [{tool_name}] on: {payload}"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
